5a97486d91d465c5964241ef8a8815d907910908
spartannik <dead5042@gmail.com>
2022-08-31 14:34:19 +0300

    completed

---
+++ TestTask/TableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  TableViewCell.swift
+//  TestTask
+//
+//  Created by Nikita Yashchenko on 31.08.2022.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    @IBOutlet weak var stringLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- TestTask/ViewController.swift
+++ TestTask/ViewController.swift
@@ -7,13 +7,65 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var titleTF: UITextField!
+    @IBOutlet weak var yearTF: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var titleList: [String] = []
+    var yearList: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        createTable()
+        yearTF.delegate = self
     }
+    
+    func createTable() {
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if textField == yearTF {
+                    let allowedCharacters = "1234567890"
+                    let allowedCharacterSet = CharacterSet(charactersIn: allowedCharacters)
+                    let typedCharacterSet = CharacterSet(charactersIn: string)
+                    let alphabet = allowedCharacterSet.isSuperset(of: typedCharacterSet)
+                    return alphabet
 
 
+          }
+        return true
+      }
+
+    
+    @IBAction func addButton(_ sender: UIButton) {
+        if titleList.contains(titleTF.text ?? "") {
+            titleTF.text = "this movie is added"
+            yearTF.text = ""
+        } else {
+            titleList.append(titleTF.text ?? "")
+            yearList.append(yearTF.text ?? "")
+            tableView.reloadData()
+            titleTF.text = ""
+            yearTF.text = ""
+        }
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell",
+                                                 for: indexPath) as! TableViewCell
+        cell.stringLabel.text = "\(titleList[indexPath.row]) \(yearList[indexPath.row])"
+        return cell
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return titleList.count
+    }
 }
 
